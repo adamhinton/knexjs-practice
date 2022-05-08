956baddf0aeacab916ec949dc8fbba7d43474fe9
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 04:25:33 -0400

    create id increment, comment on it

--- db/db/migrations/20220508082309_init.js
+++ db/db/migrations/20220508082309_init.js
@@ -2,14 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("person", (table) => {
+    //column called id auto increments
+    table.increments("id");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
