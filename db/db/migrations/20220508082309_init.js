2fac77837098ba65101af6348c1e0dea38e6308e
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 04:28:50 -0400

    put timestamps in table

--- db/db/migrations/20220508082309_init.js
+++ db/db/migrations/20220508082309_init.js
@@ -10,6 +10,7 @@ exports.up = function (knex) {
     table.string("email").notNullable().unique();
     table.string("first_name").notNullable();
     table.string("last_name").notNullable();
+    table.timestamps(true, true);
   });
 };
 
