c1be93b0862520fdaf2cdefee053c3c2906820d5
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 05:05:21 -0400

    add more detail to controller createPerson

--- controller/person.js
+++ controller/person.js
@@ -1,6 +1,15 @@
+const personService = require("../service/person");
+
 //Can also just do this the way I was taught at lambda
 class PersonController {
-  createPerson(req, res) {}
+  async createPerson(req, res) {
+    try {
+      const id = await personService.createPerson(req.body);
+      res.status(201).json(id);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 module.exports = new PersonController();
