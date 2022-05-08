55f84111ff7ca47eea8b17b664bf21e916d1cd16
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 04:59:01 -0400

    flesh out createPerson in service/person.js

--- service/person.js
+++ service/person.js
@@ -1,7 +1,10 @@
 const PersonDAO = require("../dao/person");
 
 class PersonService {
-  createPerson(firstName, lastName, email) {}
+  createPerson(personDto) {
+    const { firstName, lastName, email } = personDto;
+    PersonDAO.createPerson(firstName, lastName, email);
+  }
 }
 
 module.exports = new PersonService();
