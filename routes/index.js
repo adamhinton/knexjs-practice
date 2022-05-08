ab2c15f3521ee56ba66d94c6af5ec218c97fd182
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 05:07:08 -0400

    successfully start server with post ability

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const PersonController = require("../controller/person");
 
 const router = express.Router();
 
 //Tutorial wants me to import this logic, I can also just do this the way I was taught at Lambda with req res etc
-router.post("/person");
+router.post("/person", PersonController.createPerson);
 
 module.exports = router;
