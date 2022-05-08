65f2d47adb0ce30e38bd40ae41626a500244daff
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 04:26:17 -0400

    create email column

--- db/db/migrations/20220508082309_init.js
+++ db/db/migrations/20220508082309_init.js
@@ -5,7 +5,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("person", (table) => {
     //column called id auto increments
+    //youtuber says this may not be the best way, something to do with primary keys
     table.increments("id");
+    table.string("email").notNullable().unique();
   });
 };
 
